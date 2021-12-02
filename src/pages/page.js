b256c9558f39dc8195b590f2166bdc199225ccd2
Aleksei Sorokin <aso@anadeainc.com>
2021-12-02 16:26:08 +0300

    new page

---
+++ src/pages/page.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Link } from 'gatsby';
+
+const page = () => (
+    <div>
+        <h1>qwe</h1>
+        <p>test</p>
+        <Link to="/">to main</Link>
+    </div>
+)
+
+export default page;
