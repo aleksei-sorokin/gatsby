ed0533172bed45d3a31409a26e4b6aa7a1d29ce7
Aleksei Sorokin <aso@anadeainc.com>
2021-12-16 11:49:07 +0300

    test spam filter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,51 +1,42 @@
-import * as React from 'react'
-import { Formik, Form } from 'formik'
+import * as React from 'react';
+import { useFormik } from 'formik'
 
-function encode(data) {
+const encode = (data) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+    .join('&');
+};
 
 const Contact = () => {
+  const formik = useFormik({
+    initialValues: {
+      'bot-field': '',
+      'form-name': 'contact',
+      message: '',
+    },
+    onSubmit: (values) => {
+      console.log('values', values);
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+          'form-name': 'formik',
+          ...values,
+        }),
+      })
+        .then((succ) => console.log('succ', succ))
+        .catch((error) => console.log(error));
+    },
+  });
+
   return (
-    <Formik
-      initialValues={{
-        'bot-field': '',
-        'form-name': 'contact',
-        message: '',
-      }}
-      onSubmit={(values) => {
-        fetch('/', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: encode({
-            'form-name': 'formik',
-            ...values,
-          }),
-        })
-          .then((succ) => console.log('succ', succ))
-          .catch((error) => console.log(error))
-      }}
-      render={({ handleChange, values }) => (
-        <Form
-          name="contact"
-          method="post"
-          data-netlify-honeypot="bot-field"
-          data-netlify="true"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <input
-            type="text"
-            name="message"
-            value={`${values.firstName}`}
-            onChange={handleChange}
-            label="Name"
-          />
-          <button title="Submit" type="submit" >send</button>
-        </Form>
-      )}
-    />
-  )
-}
-export default Contact
+    <form name='contact' method='post' data-netlify-honeypot='bot-field' data-netlify='true' onSubmit={formik.handleSubmit}>
+      <input type='hidden' name='form-name' value='contact' />
+      <input type='text' name='message' value={formik.values.message} onChange={formik.handleChange} label='Name' />
+      <button title='Submit' type='submit'>
+        send
+      </button>
+    </form>
+  );
+};
+export default Contact;
