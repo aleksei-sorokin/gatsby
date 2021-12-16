62488ebc78f17f919ea7d3cdfdc4be114c3e1be1
Aleksei Sorokin <aso@anadeainc.com>
2021-12-16 11:09:43 +0300

    test form

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,28 +63,30 @@ const IndexPage = ({ data }) => {
       </div>
       <div className='result'></div>
 
-      <form 
-      name='contact' 
-      method='POST' 
-      netlify-honeypot='bot-field' 
-      data-netlify='true'>
-        <p style={{display: 'none'}}>
+      <form name='contact' method='POST' data-netlify='true'>
+        <p>
           <label>
-            Don’t fill this out if you’re human: <input name='bot-field' />
+            Your Name: <input type='text' name='name' />
           </label>
         </p>
         <p>
           <label>
-            Email: <input type='text' name='email' />
+            Your Email: <input type='email' name='email' />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{' '}
+            <select name='role[]' multiple>
+              <option value='leader'>Leader</option>
+              <option value='follower'>Follower</option>
+            </select>
           </label>
         </p>
         <p>
           <label>
             Message: <textarea name='message'></textarea>
           </label>
-        </p>
-        <p>
-          <div data-netlify-recaptcha="true"></div>
         </p>
         <p>
           <button type='submit'>Send</button>
